4111097597d49236cf9eaf6f41b3c02adcb046dc
FerhadDev <ferhadzamanov0@gmail.com>
2025-05-01 14:46:47 +0400

    Tengri 0.0.0.3
    MainMenu üzərində işləndi . Oyun başlamaq , oyundan çxmaq , ayarlara getmək , creditsə getmək ,  rules ekranlarına getmək və çıxmaq olur.

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject _SettingMenu;
+    [SerializeField] private GameObject _MainMenu;
+    [SerializeField] private GameObject _RulesMenu;
+    [SerializeField] private GameObject _CreditsMenu;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Hal haz;rda oldugun sceneden sonra gelen sceneye kecir.
+    }
+    public void GoToSettingMenu()
+    {
+        _SettingMenu.SetActive(true);
+    }
+    public void GoToRulesMenu()
+    {
+        _RulesMenu.SetActive(true);
+    }
+    public void GoToCreditsMenu()
+    {
+        _CreditsMenu.SetActive(true);
+    }
+    public void GoToMainMenu()
+    {
+        _SettingMenu.SetActive(false);
+        _CreditsMenu.SetActive(false);
+        _RulesMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+}
